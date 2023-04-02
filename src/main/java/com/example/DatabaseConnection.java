54f56bb360b350b18fd39769107cb5365f6e02e9
itzgabe15 <gzaratea@uncc.edu>
2023-04-02 00:59:42 -0400

    testing

---
+++ src/main/java/com/example/DatabaseConnection.java
@@ -0,0 +1,49 @@
+package main.java.com.example;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://capstoneproject.ck3v6fmgoed4.us-east-2.rds.amazonaws.com/PasswordManager";
+            String username = "admin";
+            String password = "itsc4155group3";
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet executeQuery(String query) {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public void closeConnection() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
